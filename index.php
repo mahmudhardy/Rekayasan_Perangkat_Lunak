3e8b62410c64e2abe4b70fe83ba8634e33991de5
MAHMUD <72733031+mahmudhardy@users.noreply.github.com>
2023-12-08 23:05:04 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en" class="fullscreen-bg">
+
+<head>
+    <title>Drinker</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+	<!-- MAIN CSS -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
+	<link rel="stylesheet" href="assets/css/demo.css">
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- Koneksi -->
+	<?php
+	include 'admin/konfig.php';
+	?>
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/Coffee_Icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/Coffee_Icon.png">
+	<!-- BACKGROUND-->
+	<style>
+		body{
+			background-image:url("assets/img/bgd.png");
+			background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            height: 100%;
+		}
+	</style>
+</head>
+<body>
+	<!-- cek login -->
+	<?php
+	if (isset($_GET['pesan'])) {
+		if ($_GET['pesan'] == "Gagal") {
+			echo "<div style='margin-bottom:-55px' class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span>  Login Gagal !! Username dan Password Salah !!</div>";
+		}
+	}
+	?>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box" align="center" class="tengah">
+					<div class="content">
+						<div class="header">
+							<div class="col-md-12">
+								<img src="assets/img/Coffe_go.png" class="ban-img img-fluid">
+							</div>
+							<p class="lead">Halaman Login</p>
+						</div>
+						<form class="form-auth-small" action="cek_login.php" method="post">
+							<div class="form-group">
+								<label for="signin-email" class="control-label sr-only">Username</label>
+								<input align="center" type="text" class="form-control" name="username" placeholder="Username">
+							</div>
+							<div class="form-group">
+								<label for="signin-password" class="control-label sr-only">Password</label>
+								<input align="center" type="password" class="form-control" name="password" placeholder="Password">
+							</div>
+							<button type="submit" class="btn btn-primary btn-lg btn-block" name="btnLogin">LOGIN</button>
+							<div class="bottom">
+								<span class="helper-text">Belum Punya Akun, Silahkan <a href="daftar.php">Daftar</a></span>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+</html>
---
+++ index_awal.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Drinker</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Free landing page website template">
+	<meta name="author" content="The Develovers">
+
+	<!-- CORE CSS -->
+	<link href="assets/css/tampilan_bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	
+	<!-- THEME CSS -->
+	<link href="assets/css/tampilan_main.css" rel="stylesheet" type="text/css">
+	
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700%7CRoboto:300,400,700" rel="stylesheet">
+	
+	<!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/Coffee_Icon.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/Coffee_Icon.png">
+</head>
+<body data-spy="scroll">
+	
+	<!-- WRAPPER -->
+	<div id="wrapper">
+
+		<!-- HERO SECTION -->
+		<section id="home" class="hero-unit-fullscreen parallax-window" data-parallax="scroll" data-image-src="assets/img/fullscreen.png">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="hero-content">
+					<h1 class="hero-heading">WELCOME</h1>
+					<p class="lead">Save your time and money. Get your business started.</p>
+					<span class="onepage-links"><a href="index.php" class="btn btn-lg btn-primary">LEARN MORE</a></span>
+				</div>
+			</div>
+		</section>
+		<!-- END HERO SECTION -->
+
+		<!-- OVERVIEW -->
+		<section id="overview">
+			<div class="container">
+				<div class="section-heading">
+					<h2 class="heading">OVERVIEW</h2>
+				</div>
+				<p class="text-center lead">website ini dibuat untuk pemilik bisnis atau kreator untuk memulai atau meningkatkan visibilitas minuman coklat mereka</p>
+
+				<div class="row">
+					<div class="col-md-4">
+						<div class="icon-info icon-info-center">
+							<i class="fa fa-thumbs-o-up"></i>
+							<h3 class="title">LOCAL CULTURE</h3>
+							<p>mendaftar untuk keanggotaan hanyalah permulan dan kami akan mengirim email kepada Anda detail menarik saat minuman coklat di kota majene Anda selesai.</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="icon-info icon-info-center">
+							<i class="fa fa-laptop"></i>
+							<h3 class="title">ALL TOURS ARE FREE</h3>
+							<p>mengarungi minuman coklat yang membuat Anda bersemangat?. sehari sebelum acara kami akan mengirimi Anda email dan mengungkapkan lokasi minuman coklat Anda.</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="icon-info icon-info-center">
+							<i class="fa fa-star-o"></i>
+							<h3 class="title">TRUST AND SAFETY</h3>
+							<p>Anda ada di daftar! minum. silahkan duduk dan Nikmati memberi Anda pengalaman terbaik</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END OVERVIEW -->
+
+		<!-- TEAM -->
+		<section id="team">
+			<div class="container">
+				<div class="section-heading">
+					<h2 class="heading">TEAM</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<div class="team-member">
+							<img src="assets/img/person1.png" class="img-responsive img-circle" alt="Person">
+							<div class="member-info">
+								<h3 class="name">Bambang Manggala</h3>
+								<span class="title text-muted">D0220344</span>
+								<p class="short-bio">---</p>
+								<ul class="list-inline social-icons">
+									<li><a href="#" class="facebook-bg"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="twitter-bg"><i class="fa fa-twitter"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="team-member">
+							<img src="assets/img/person2.png" class="img-responsive img-circle" alt="Person">
+							<div class="member-info">
+								<h3 class="name">Sahara</h3>
+								<span class="title text-muted">D0220336</span>
+								<p class="short-bio">---</p>
+								<ul class="list-inline social-icons">
+									<li><a href="#" class="facebook-bg"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="twitter-bg"><i class="fa fa-twitter"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="team-member">
+							<img src="assets/img/person3.png" class="img-responsive img-circle" alt="Person">
+							<div class="member-info">
+								<h3 class="name">Mahmud</h3>
+								<span class="title text-muted">D0220520</span>
+								<p class="short-bio">---</p>
+								<ul class="list-inline social-icons">
+									<li><a href="#" class="facebook-bg"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#" class="twitter-bg"><i class="fa fa-twitter"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END TEAM -->
+
+		<!-- CONTACT -->
+		<section id="contact">
+			<div class="container">
+				<div class="section-heading">
+					<h2 class="heading">CONTACT</h2>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						<p>
+							<strong><i class="icon icon_pin_alt"></i> Alamat</strong>
+							<br>
+							<span>Majene<br>Sulawesi Barat</span>
+						</p>
+						<br>
+						<p>
+							<strong><i class="icon icon_phone"></i> No HP</strong>
+							<br>
+							<span>Phone 1: 1-(558) 968-0400</span>
+							<br>
+							<span>Phone 2: 1-(558) 968-1234</span>
+						</p>
+						<br>
+						<p>
+							<strong><i class="icon icon_mail"></i> Email</strong>
+							<br>
+							<span>Email  : <a href=drinker@yourdomain.com">drinker@yourdomain.com</a></span>
+						</p>
+					</div>
+					<div class="col-md-8">
+						<form id="contact-form" class="form-horizontal form-minimal">
+							<div class="row">
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="contact-name" class="control-label sr-only">Name</label>
+										<input type="text" class="form-control" id="contact-name" placeholder="Name (required)" required>
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<div class="form-group">
+										<label for="contact-email" class="control-label sr-only">Email</label>
+										<input type="email" class="form-control" id="contact-email" placeholder="Email (required)" required>
+									</div>
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="contact-subject" class="control-label sr-only">Subject</label>
+								<div class="col-sm-12">
+									<input type="text" class="form-control" id="contact-subject" placeholder="Subject (optional)">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="contact-message" class="control-label sr-only">Message</label>
+								<div class="col-sm-12">
+									<textarea class="form-control" id="contact-message" name="contact-message" rows="5" cols="30" placeholder="Message (required)" required></textarea>
+								</div>
+							</div>
+							<div class="form-group">
+								<div class="col-sm-12">
+									<button type="submit" class="btn btn-primary">Submit Message</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END CONTACT -->
+
+		<!-- FOOTER -->
+		<footer class="footer-minimal">
+			<div class="container">
+				<nav>
+					<ul class="list-unstyled list-inline margin-bottom-30px">
+						<li><a href="#">About</a></li>
+						<li><a href="#">Terms &amp; Condition</a></li>
+						<li><a href="#">Help</a></li>
+					</ul>
+				</nav>
+				<ul class="list-inline social-icons social-icons-circle">
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				</ul>
+				<p class="copyright-text">Designed &amp; Crafted by <a href="#" target="_blank">Mahasiswa Universitas Sulawesi Barat</a></p>
+			</div>
+		</footer>
+		<!-- END FOOTER -->
+
+	</div>
+	<!-- END WRAPPER -->
+
+	<!-- JAVASCRIPT -->
+	<script src="assets/js/jquery-2.1.1.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/plugins/scrolling/jquery.scrollTo-1.4.3.1-min.js"></script>
+	<script src="assets/js/plugins/scrolling/jquery.localscroll-1.2.7-min.js"></script>
+	<script src="assets/js/plugins/parallax/parallax.min.js"></script>
+	<script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+	<script src="assets/js/landee.js"></script>
+	
+</body>
+
+</html>
